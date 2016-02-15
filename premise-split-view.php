287dc297a576f4645dd76d09555bed038fea3206
François Jacquet <francoisjacquet@users.noreply.github.com>
2016-02-15 13:01:54 -0500

    Add TGM plugin activation require Premise-WP

--- premise-split-view.php
+++ premise-split-view.php
@@ -158,6 +158,16 @@ class Premise_Split_View {
 	 * Include all necessary files for our plugin to work properly.
 	 */
 	public function includes() {
+
+		// Require Premise WP.
+		if ( ! class_exists( 'Premise_WP' ) ) {
+
+			// Require Premise WP plugin with the help of TGM Plugin Activation.
+			require_once PREMISE_SPLITV_PATH . 'TGM-Plugin-Activation/class-tgm-plugin-activation.php';
+
+			add_action( 'tgmpa_register', array( $this, 'psview_register_required_plugins' ) );
+		}
+
 		include 'class-cpt-ui.php';
 		include 'class-render.php';
 		include 'class-shortcode.php';
@@ -197,5 +207,58 @@ class Premise_Split_View {
 	}
 
 
+
+
+
+
+
+
+	/**
+	 * Register the required plugins for this theme.
+	 *
+	 * We register one plugin:
+	 * - Premise-WP from a GitHub repository
+	 *
+	 * @link https://github.com/PremiseWP/Premise-WP
+	 */
+	function psview_register_required_plugins() {
+		/*
+		 * Array of plugin arrays. Required keys are name and slug.
+		 * If the source is NOT from the .org repo, then source is also required.
+		 */
+		$plugins = array(
+
+			// Include Premise-WP plugin.
+			array(
+				'name'               => 'Premise-WP', // The plugin name.
+				'slug'               => 'Premise-WP', // The plugin slug (typically the folder name).
+				'source'             => 'https://github.com/PremiseWP/Premise-WP/archive/master.zip', // The plugin source.
+				'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+				// 'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+				'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+				// 'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+				// 'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+				// 'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+			),
+		);
+
+		/*
+		 * Array of configuration settings.
+		 */
+		$config = array(
+			'id'           => 'psview-tgmpa',         // Unique ID for hashing notices for multiple instances of TGMPA.
+			'default_path' => '',                      // Default absolute path to bundled plugins.
+			'menu'         => 'tgmpa-install-plugins', // Menu slug.
+			'parent_slug'  => 'plugins.php',            // Parent menu slug.
+			'capability'   => 'install_plugins',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+			'has_notices'  => true,                    // Show admin notices or not.
+			'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
+			'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+			'is_automatic' => true,                   // Automatically activate plugins after installation or not.
+			'message'      => '',                      // Message to output right before the plugins table.
+		);
+
+		tgmpa( $plugins, $config );
+	}
+
 }
-?>
